933422ec73a348a6e80a061a22acbbef8ae08c09
Lucas Souza <79222458+lucasdev3@users.noreply.github.com>
2022-03-03 00:27:54 -0300

    iniciando servidor base

--- dev-repo-server/src/app.js
+++ dev-repo-server/src/app.js
@@ -1,8 +1,27 @@
-import express from "express"
+import express, { Router } from "express"
 import cors from "cors"
 
 class App {
-    
+    constructor() {
+        this.server = express()
+        this.middlewares()
+        this.routes()
+    }
+
+    middlewares() {
+        this.server.use(express.json())
+        this.server.use(cors())
+    }
+
+    routes() {
+        const routes = new Router()
+
+        routes.get('/hello', (req, res) => {
+            res.json({ hello: 'world' })
+        })
+
+        this.server.use(routes)
+    }
 }
 
 export default new App().server
